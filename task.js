77a1ba839210a93ed97f092752b862c54177cf04
Maria <dreiida@mail.ru>
2020-01-04 20:30:02 +0300

    test commit

---
+++ task.js
@@ -0,0 +1,33 @@
+//'use strict';
+
+
+let money = prompt("Ваш бюджет на месяц?", ""),
+    time = prompt("Введите дату в формате DD-MM-YYYY", "");
+
+let appData = {
+    budget: money,
+    timeData: time,
+    expenses: {},
+    optionalExpenses: {
+
+    },
+    income: [
+
+    ],
+    savings: false
+};
+
+let a1 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+	a2 = prompt("Во сколько обойдется?", ''),
+	a3 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+    a4 = prompt("Во сколько обойдется?", '');
+
+    appData.expenses.a1 = a2;
+    appData.expenses.a3 = a4;
+
+let result; 
+result = Number(a2) + Number(a4);
+//alert(result);
+appData.budget = appData.budget - result;
+    
+alert(appData.budget / 30);
