dfdcac6a89d9ad1b15d5094177ceb2700a587fcc
Maria <dreiida@mail.ru>
2020-01-04 20:44:48 +0300

    right commit

---
+++ lesson10.js
@@ -0,0 +1,39 @@
+
+/*
+let i=1;
+while (i<8){
+console.log (i);
+    i++;
+}
+*/
+/*
+let i=1;
+
+while (i<8){
+if (i==6){
+break
+}
+console.log (i);
+    i++;
+}
+*/
+/*
+let i=1;
+while (i<8){
+if (i==6){
+continue
+}
+console.log (i);
+    i++;
+}
+*/
+/*
+let i=1;
+do {
+    console.log (i);
+    i++;
+    }
+    while (i<8);
+    */
+
+
